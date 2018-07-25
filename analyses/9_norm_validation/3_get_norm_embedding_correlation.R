500e721a310009aaed7415d6c5b755a0f045a342
Molly Lewis <mollyllewis@gmail.com>
2018-07-25 10:56:50 -0500

    cogsci poster

--- analyses/9_norm_validation/3_get_norm_embedding_correlation.R
+++ analyses/9_norm_validation/3_get_norm_embedding_correlation.R
@@ -21,15 +21,19 @@ all_ratings <- embedding_ratings %>%
   left_join(glasgow_norms, by = c("word2" = "word"))
 
 ggplot(all_ratings, aes(x = norm_maleness, y = male_score)) +
-  geom_point(size = .2) +
-  xlab("Glasglow gender norm rating (maleness)") +
-  ylab("Word embedding gender score (maleness)") +
+  #geom_point(size = .4) +
+  geom_text(aes(label = word2), size = 1) +
+  xlab("Explicit gender norm rating\n(maleness)") +
+  ylab("Word embedding gender score\n (maleness)") +
+  ggtitle("Explicit vs. Language-Embedding \nword gender bias") +
   geom_smooth(method = "lm") +
-  annotate("text", label = "r = .59", x = 2, y = .2, color = "red") +
-  theme_classic()
+  annotate("text", label = "r = .59", x = 6, y = -.2, 
+           color = "red", size = 18) +
+  theme_classic() +
+  theme(text = element_text(size = 26),
+        axis.line = element_line(colour = "black", size = 1.2)) 
 
 cor.test(all_ratings$norm_maleness, all_ratings$male_score)
-
 cor.test(all_ratings$female_targets, all_ratings$male_targets)
 cor.test(all_ratings$female_targets, all_ratings$norm_maleness)
 cor.test(all_ratings$male_targets, all_ratings$norm_maleness)
