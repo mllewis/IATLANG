fb5939ca3f3523377aca3bd743563dbcc9dfb468
Molly Lewis <mollyllewis@gmail.com>
2018-08-17 14:43:30 -0500

    science pi analyses

--- analyses/8_gender_genius/2_language_wikipedia/3_save_subsetted_models.R
+++ analyses/8_gender_genius/2_language_wikipedia/3_save_subsetted_models.R
@@ -3,13 +3,14 @@ library(tidyverse)
 library(data.table)
 
 
-INFILE <- "tidy_iat_translations2.csv"
+INFILE <- "data/tidy_iat_translations.csv"
 LANGKEY <- "../../../data/other/country_langiso_langwiki_key.csv"
 MODEL_PREFIX <- "/Volumes/wilbur_the_great/fasttext_models/wiki."
 
 lang_key <- read_csv(LANGKEY) %>%
   mutate(language = tolower(language_name)) %>%
-  select(language_name, wiki_language_code) %>%
+  select(language, wiki_language_code)%>%
+  add_row(language = "tamil", wiki_language_code = "ta") %>%
   distinct()
 
 translations <- read_csv(INFILE)  %>%
@@ -39,22 +40,24 @@ save_subsetted_model <- function(current_lang, trans_df, model_prefix){
           by = "translation", all.x = TRUE)
   
   # write raw vectors
-  relevant_raw_vector_path <- "subsetted_models/raw/"
+  relevant_raw_vector_path <- "data/subsetted_models/raw/"
   write_csv(relevant_vectors, paste0(relevant_raw_vector_path, "wiki.", current_lang, "_raw.csv"))
   
   calculated_vectors <- relevant_vectors %>%
     select(-translation) %>%
-    group_by(language_code, word, gender, translation_id) %>% # sum across word ids
+    group_by(language_code, word, gender, translation_id) %>% # sum across word ids/ or mean?
+    #summarise_at(vars(V2:V301), mean, na.rm = TRUE) %>%
     summarise_at(vars(V2:V301), sum, na.rm = TRUE) %>%
     group_by(language_code, word, gender) %>% 
     summarize_at(vars(V2:V301), mean, na.rm = TRUE) # mean across words
   
   # write calculated vectors
-  relevant_calculated_vector_path <-  "subsetted_models/calculated/"
+  relevant_calculated_vector_path <-  "data/subsetted_models/calculated/"
   write_csv(calculated_vectors, paste0(relevant_calculated_vector_path, "wiki.", 
-                                       current_lang, "_calculated.csv"))
+                                       current_lang, "_calculated_sum.csv"))
   
 }
 
 # get all subsetted models
-walk(unique(translations$wiki_language_code), save_subsetted_model, translations, MODEL_PREFIX)
+#unique(translations$wiki_language_code),
+walk("en", save_subsetted_model, translations, MODEL_PREFIX)
--- analyses/8_gender_genius/2_language_wikipedia/4_calculated_es.R
+++ analyses/8_gender_genius/2_language_wikipedia/4_calculated_es.R
@@ -2,17 +2,21 @@
 library(tidyverse)
 source("IAT_utils2.R")
 
-LANGS <- c("fa", "de", "nl", "pt", "zh", "es", "id", "da", "it", "ko", "ru", "tr", "he", "ro", "ja", "ar", "fi", 
-           "fr", "ms", "pl", "sv")
+
+LANGS2  <- c("fa", "de", "nl", "zh", "es", "id", "da", "it", "ko", "ru", "tr", "he", "ro", "ja", "ar", "fi", 
+             "fr",  "pl",  "no", "sk", "ta", "ur", "pt", "en") #sw #ms
+
+OUTFILE_GENIUS <- "data/es_genius_separate_pro_sum.csv"
+
 
 #### loop over langs and get effect sizes #### 
 get_wiki_es_separate_gender <- function(current_lang, word_list){
   
   # read back in subsetted file
-  relevant_calculated_vector_path <- "subsetted_models/calculated/"
+  relevant_calculated_vector_path <- "data/subsetted_models/calculated/"
   
   subsetted_model <- read_csv(paste0(relevant_calculated_vector_path, 
-                                     "wiki.", current_lang, "_calculated.csv")) %>%
+                                     "wiki.", current_lang, "_calculated_sum.csv")) %>%
     select(-language_code) %>%
     mutate(word = str_replace_all(word, "-", " ")) %>%
     rename(target_word = word)
@@ -38,7 +42,7 @@ get_wiki_es_separate_gender <- function(current_lang, word_list){
       filter(gender %in% c("M", "N")) %>%
       select(-gender)
     
-    ES_M<- get_swabs_only(word_list, subsetted_model_m, "M") %>%
+    ES_M <- get_swabs_only(word_list, subsetted_model_m, "M") %>%
       mutate(language_code = current_lang,
              gender = "M") %>%
       select(language_code, everything()) 
@@ -57,25 +61,21 @@ get_wiki_es_separate_gender <- function(current_lang, word_list){
   
 }
 
+### Genius ###
 
-### Career ###
-OUTFILE_CAREER <- "es_career_separate.csv"
+word_list_genius <- list(test_name = "genius_gender", 
+                         bias_type = "genius_gender",
+                         category_1 = c("male", "man", "he", "him", "his"),
+                         category_2 = c("female", "woman",  "she", "her", "hers"),
+                         #category_1 = c("male", "man"),
+                         #category_2 = c("female", "woman"),
+                         attribute_1 = c("genius", "brilliant", "super smart"),
+                         attribute_2 = c("creative", "artistic", "super imaginative"))
 
-word_list_career <- list(test_name = "WEAT_6", # not identical to caliskan (caliskan used proper names)
-                    bias_type = "gender-bias-career-family",
-                    category_1 = c("male", "man", "boy", "brother", "he", "him", "his", "son"),
-                    category_2 = c("female", "woman", "girl", "sister", "she", "her", "hers", "daughter"),
-                    #category_1 = c("male", "man", "boy", "brother","son"),
-                    #category_2 = c("female", "woman", "girl", "sister",  "daughter"),
-                    attribute_1 = c("executive", "management", "professional", "corporation", "salary", 
-                                    "office", "business", "career"),
-                    attribute_2 = c("home", "parents", "children", "family", "cousins", "marriage", 
-                                    "wedding", "relatives"))
+swabs_genius <- map_df(LANGS2, get_wiki_es_separate_gender, 
+                       word_list_genius)
 
-swabs_career <- map_df(LANGS, get_wiki_es_separate_gender, 
-                word_list_career)
-
-es_career_swabs_career <- swabs_career %>%
+es_genius_swabs_genius <- swabs_genius %>%
   filter(!(category_type == "category_1" & attribute == "F")) %>%
   filter(!(category_type == "category_2" & attribute == "M")) %>%
   select(-attribute, -gender) %>%
@@ -84,8 +84,8 @@ es_career_swabs_career <- swabs_career %>%
   summarize_all(mean, na.rm = T) %>%
   mutate(sYXab_num = category_1 - category_2,
          sYXab = sYXab_num/sd)  %>%
-  mutate(test = pluck(word_list_career, "test_name"), 
-        bias_type = pluck(word_list_career, "bias_type")) %>%
+  mutate(test = pluck(word_list_genius, "test_name"), 
+         bias_type = pluck(word_list_genius, "bias_type")) %>%
   select(language_code, test, bias_type, sYXab)
 
-write_csv(es_career_swabs_career, OUTFILE_CAREER)
+write_csv(es_genius_swabs_genius, OUTFILE_GENIUS)
