ff18b6e9023f1fe8224d43ae9f2c7778e190679f
Molly Lewis <mollyllewis@gmail.com>
2019-05-27 21:22:10 -0500

    submitted pnas version

--- analyses/other/01_tidy_other_psych_measure_data.R
+++ analyses/other/01_tidy_other_psych_measure_data.R
@@ -8,12 +8,22 @@ STOET_PATH <- here("data/other/stoet_geary_2018_data.csv")
 FALK_PATH <- here("data/other/falk_hermle_2018_data.dta")
 LANGUAGE_COUNTRY_IN <- here("data/study0/processed/top_lang_by_country_ethnologue.csv")
 LANG_IAT_PATH <- here("data/study1b/iat_es_lang.csv")
+PSYCH_IAT_PATH <- here("data/study0/processed/by_country_df.csv")
+
 
 OUTFILE <- here("data/other/tidy_other_psych_measures.csv")
 
+# get GDP 2017 data from World Bank API
+gdp_data <- wbstats::wb(indicator = "NY.GDP.PCAP.CD", 
+                        startdate = 2017, 
+                        enddate = 2017) %>%
+  select(iso2c, value) %>%
+  rename(gdp_2017 = value,
+         country_code = iso2c)
+
 stoet_data <- read_csv(STOET_PATH) %>%
   mutate(country_code = countrycode(country_name, "country.name", "iso2c")) %>%
-  select(country_code, self_efficacy_diff, intra_indv_diff) %>%
+  select(per_women_stem, country_code, self_efficacy_diff, intra_indv_diff) %>%
   rename(self_efficacy_diff_sg = self_efficacy_diff, 
          intra_indv_diff_sg = intra_indv_diff)
 
@@ -30,11 +40,15 @@ other_data <- full_join(stoet_data, falk_data) %>%
   full_join(unique_langs_per_country)
 
 iat_lang_es <- read_csv(LANG_IAT_PATH)
+psych_iat_es <- read_csv(PSYCH_IAT_PATH) %>%
+  select(country_code, es_iat_sex_age_order_implicit_resid)
 
 tidy_other_data <- other_data %>%
   filter(!is.na(language_code),
          language_code != "zu") %>%
   left_join(iat_lang_es) %>%
+  left_join(gdp_data) %>%
+  left_join(psych_iat_es) %>%
   mutate(country_name = countrycode(country_code, "iso2c","country.name"))
 
 write_csv(tidy_other_data, OUTFILE)
