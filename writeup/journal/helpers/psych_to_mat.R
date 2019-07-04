f25433f4f8aadceefe6fa1a0a0d1d50d258c8ced
Molly Lewis <mollyllewis@gmail.com>
2019-07-04 10:22:08 -0400

    fixing NHB version

--- writeup/journal/helpers/psych_to_mat.R
+++ writeup/journal/helpers/psych_to_mat.R
@@ -9,3 +9,70 @@ psych_to_mat <- function(psych_thing){
   }
   new_mat
 }
+
+tidy_r_to_text_r <- function(tidy_row){
+  round_tidy <- tidy_row %>%
+    mutate_at(vars(estimate, contains("conf")), round, 2)
+  
+  p_value <- case_when(pull(round_tidy, p.value) < .0001 ~ "_p_ < .0001",
+                       pull(round_tidy, p.value) < .001 ~ "_p_ < .001",
+                       TRUE ~ paste0("_p_ = ", round(pull(round_tidy, p.value),2)))
+  
+  paste0(
+    "_r_(",
+    pull(round_tidy, parameter),
+    ") = ", 
+    pull(round_tidy, estimate),
+    " [",
+    pull(round_tidy, conf.low),
+    ", ",
+    pull(round_tidy, conf.high),
+    "], ",
+    p_value
+  )
+}
+
+tidy_t_to_text_t <- function(tidy_row){
+  
+  round_tidy <- tidy_row %>%
+    mutate_at(vars(statistic, estimate, contains("conf"), parameter), round, 2)
+  
+  p_value <- case_when(pull(round_tidy, p.value) < .0001 ~ "_p_ < .0001",
+                       pull(round_tidy, p.value) < .001 ~ "_p_ < .001",
+                       TRUE ~ paste0("_p_ = ", round(pull(round_tidy, p.value),2)))
+  
+  
+  
+  paste0(
+    "_M_ = ", 
+    pull(round_tidy, estimate),
+    " [",
+    pull(round_tidy, conf.low),
+    ", ",
+    pull(round_tidy, conf.high),
+    "]; ",
+    "_t_(",
+    pull(round_tidy, parameter),
+    ") = ", 
+    pull(round_tidy, statistic),
+    ", ",
+    p_value
+  )
+  
+  
+}
+
+
+cohen_d_to_text_d <- function(d_obj){
+  
+  paste0(
+    "_d_ = ", 
+    round(d_obj$estimate, 2),
+    " [",
+    round(pluck(d_obj$conf.int,1), 2),
+    ", ",
+    round(pluck(d_obj$conf.int,2), 2),
+    "]"
+  )
+  
+}
