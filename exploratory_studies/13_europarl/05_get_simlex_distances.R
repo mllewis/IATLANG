91c0029262435641e8d4d1e3a0c5594c2cf6cdef
Molly Lewis <mollyllewis@gmail.com>
2020-05-05 14:20:51 -0400

    tidy repo

--- exploratory_studies/13_europarl/05_get_simlex_distances.R
+++
@@ -1,8 +0,0 @@
-# get simlex distances
-library(tidyverse)
-library(here)
-library(data.table)
-
-
-
-SIMLEX_DATA <- here("gender_ratings/SimLex-999.txt")
--- exploratory_studies/16_wiki_native/02_get_wiki_text/wiki_dump_reader/__init__.py
+++
@@ -1,2 +0,0 @@
-from .cleaner import Cleaner
-from .loader import iterate
